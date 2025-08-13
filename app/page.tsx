a3f64cb11e9063beded1b398003f934817ad1655
zzunja <mason.mccombs@my.utsa.edu>
2025-08-12 22:47:04 -0500

    adjusted margins at bottom

--- app/page.tsx
+++ app/page.tsx
@@ -30,39 +30,37 @@ export default function Home() {
 
 
   return (
-    <div className="">
-      <div className="w-95/100 text-dark dark:text-white mx-auto font-mono sm:w-[856px] text-lg">
-        <div className="mt-3 sm:mt-[15vh]">
-          <Breadcrumb currentPage='Home'/>
-        </div>
-        <div className="flex">
-          <div className="mt-2">
-            <p className="text-5xl font-bold">Mason McCombs</p>
-            <div className="flex mt-2">
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="mr-2 translate-y-[2px]"><path d="M18 5a2 2 0 0 1 2 2v8.526a2 2 0 0 0 .212.897l1.068 2.127a1 1 0 0 1-.9 1.45H3.62a1 1 0 0 1-.9-1.45l1.068-2.127A2 2 0 0 0 4 15.526V7a2 2 0 0 1 2-2z"/><path d="M20.054 15.987H3.946"/></svg>
-              <p className="text-xl">UTSA Cyber Security Student</p>
-            </div>
-            
-            <div className="flex mt-1">
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="red" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="mr-2 translate-y-[3px]"><path d="M20 10c0 4.993-5.539 10.193-7.399 11.799a1 1 0 0 1-1.202 0C9.539 20.193 4 14.993 4 10a8 8 0 0 1 16 0"/><circle cx="12" cy="10" r="3"/></svg>
-              <p className="text-xl">Texas</p>
-            </div>
-          </div>
-          <div className="ml-auto my-auto">
-            <SocialMediaButton href="https://github.com/zzunja" media="github"/>
-            <SocialMediaButton href="https://www.linkedin.com/in/mason-mccombs-a7a05b238" media="linkedin"/>
-            <ThemeSwitch/>
-          </div>
-
-
-        </div>
-
-        <p className="mt-4 mb-6">Hi, I'm Mason McCombs, an undergrad Cyber Security student currently attending UTSA. I have a strong interest in Maleware Development and Reverse Engineering. I also enjoy making website from time to time. Here are a list of projects that I have done.</p>
-  
-        <DisplayPost
-          post={post}
-        />
+    <div className="w-95/100 text-dark dark:text-white mx-auto font-mono sm:w-[856px] text-lg mb-3 sm:mb-[15vh]">
+      <div className="mt-3 sm:mt-[15vh]">
+        <Breadcrumb currentPage='Home'/>
       </div>
+      <div className="flex">
+        <div className="mt-2">
+          <p className="text-5xl font-bold">Mason McCombs</p>
+          <div className="flex mt-2">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="mr-2 translate-y-[2px]"><path d="M18 5a2 2 0 0 1 2 2v8.526a2 2 0 0 0 .212.897l1.068 2.127a1 1 0 0 1-.9 1.45H3.62a1 1 0 0 1-.9-1.45l1.068-2.127A2 2 0 0 0 4 15.526V7a2 2 0 0 1 2-2z"/><path d="M20.054 15.987H3.946"/></svg>
+            <p className="text-xl">UTSA Cyber Security Student</p>
+          </div>
+          
+          <div className="flex mt-1">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="red" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="mr-2 translate-y-[3px]"><path d="M20 10c0 4.993-5.539 10.193-7.399 11.799a1 1 0 0 1-1.202 0C9.539 20.193 4 14.993 4 10a8 8 0 0 1 16 0"/><circle cx="12" cy="10" r="3"/></svg>
+            <p className="text-xl">Texas</p>
+          </div>
+        </div>
+        <div className="ml-auto my-auto">
+          <SocialMediaButton href="https://github.com/zzunja" media="github"/>
+          <SocialMediaButton href="https://www.linkedin.com/in/mason-mccombs-a7a05b238" media="linkedin"/>
+          <ThemeSwitch/>
+        </div>
+
+
+      </div>
+
+      <p className="mt-4 mb-6">Hi, I'm Mason McCombs, an undergrad Cyber Security student currently attending UTSA. I have a strong interest in Maleware Development and Reverse Engineering. I also enjoy making website from time to time. Here are a list of projects that I have done.</p>
+
+      <DisplayPost
+        post={post}
+      />
     </div>
   );
 }
