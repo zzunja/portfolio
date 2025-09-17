9dffddf990b1d0adb60723b28da9ae6ef8a808b8
zzunja <mason.mccombs@my.utsa.edu>
2025-09-16 19:14:11 -0500

    fixed className

--- app/page.tsx
+++ app/page.tsx
@@ -47,7 +47,7 @@ export default function Home() {
             <p className="text-xl">Texas</p>
           </div>
           <div className="flex mt-1">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="oklch(62.3% 0.214 259.815)" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" class="mr-2 translate-y-[1px]"><path d="M14 2v4a2 2 0 0 0 2 2h4"/><path d="M15 18a3 3 0 1 0-6 0"/><path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7z"/><circle cx="12" cy="13" r="2"/></svg>
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="oklch(62.3% 0.214 259.815)" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="mr-2 translate-y-[1px]"><path d="M14 2v4a2 2 0 0 0 2 2h4"/><path d="M15 18a3 3 0 1 0-6 0"/><path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7z"/><circle cx="12" cy="13" r="2"/></svg>
             <a href="/resume.pdf" className="text-xl underline text-blue-300">Resume</a>
           </div>
         </div>
